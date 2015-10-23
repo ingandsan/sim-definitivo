abad363ae392f0c057facf67ab36d9fcd86ab6e4
ingandsan <ing.and.san@gmail.com>
2015-10-23 13:15:24 -0500

    cambios 23-10-15
    van los ultimos cambios al sistema SIM

--- resources/views/Tablas/tablas.blade.php
+++ resources/views/Tablas/tablas.blade.php
@@ -1,11 +1,11 @@
 
-<table>
+<table ALIGN="center" width:"650px", height: "700px">
 <tr>
 <td>	
 <?php
 include_once('inegifacil/INEGI_library.php');
 $inegi = new INEGI_library();
-
+echo "<caption align='right'><button align='right'><a href='/impre/".$variable."/' target='_blank'><IMG SRC='/images/excel-xls-icon.png' WIDTH='50px' height='50px'></a></button><button align='right' onclick='mos()'><IMG SRC='/images/DEF.png' WIDTH='50px' height='50px'></button><button align='right' onclick='mostrardiv()'><IMG SRC='/images/meta.png' WIDTH='50px' height='50px'></button></caption>";
 switch ($variable) {
 case 4:
 ?>
@@ -52,7 +52,7 @@ break;
 case 7:
 ?>
 <caption  align='center'>Producto Interno Bruto (PIB)</caption>
-<caption  align='center'>Millones de pesos a precios constantes de 2008</caption>";
+<caption  align='center'>Millones de pesos a precios constantes de 2008</caption>
 <thead  align='center' border='2px'>
 <tr width:'100%' align='center' border='2px'>
 <th scope='col' width:'33.3%' align='center' border='2px'>Año</th>
@@ -7981,3 +7981,153 @@ case 49:
 </td>
 </tr>
 </table>
+
+
+
+
+<script> 
+function mostrardiv() { 
+table = document.getElementById('flotante'); 
+table.style.display = ''; 
+} 
+function cerrar() { 
+table = document.getElementById('flotante'); 
+table.style.display='none'; 
+} 
+</script> 
+<script> 
+function mos() { 
+table = document.getElementById('flo'); 
+table.style.display = ''; 
+} 
+function cer() { 
+table = document.getElementById('flo'); 
+table.style.display='none'; 
+} 
+</script> 
+<table id="flotante" style="display:none;" ALIGN="center">
+<tr>
+<td>
+<table WIDTH="100%">
+<tr>
+<tr><b>METADATO:</b></tr>
+<td>Variable:</td>
+<?php
+		$vars=\sim\meta::where('id_variable', '=', $variable)->take(1000)->get();
+		?>
+		@foreach ($vars as $vari)
+		<?php
+		echo "<td>". $vari->var . "</td>";
+		
+?>
+
+</tr>
+<tr><td>Unidad de Medida:</td>
+<?php
+		
+		echo "<td>". $vari->um  . "</td>";
+		
+?>
+
+</tr>
+<tr><td>Tema:</td>
+<?php
+		
+		echo "<td>". $vari->tema . "</td>";
+		
+?>
+</tr>
+<tr><td>Subtema:</td>
+<?php
+		
+		echo "<td>". $vari->subtema . "</td>";
+		
+?>
+</tr>
+<tr><td>Tipo:</td>
+<?php
+		
+		echo "<td>". $vari->tip_var . "</td>";
+		
+?>
+</tr>
+<tr><td>Interpretación:</td>
+<?php
+		
+		echo "<td>". $vari->inter . "</td>";
+		
+?>
+</tr>
+<tr><td>Periodicidad:</td>
+<?php
+		
+		echo "<td>". $vari->per . "</td>";
+		
+?>
+</tr>
+<tr><td>Cobertura Temporalidad Nacional:</td>
+<?php
+		
+		echo "<td>". $vari->Cob_tem_nac . "</td>";
+		
+?>
+</tr>
+<tr><td>Cobertura Temporalidad Estatal:</td>
+<?php
+		
+		echo "<td>". $vari->Cob_tem_est . "</td>";
+		
+?>
+</tr>
+<tr><td>Cobertura Geográfica:</td>
+<?php
+		
+		echo "<td>". $vari->cob_geo . "</td>";
+		
+?>
+</tr>
+<tr><td>Fuente:</td>
+<?php
+		
+		echo "<td>". $vari->fuente . "</td>";
+		
+?>
+</tr>
+<tr><td>Nota:</td>
+<?php
+		echo "<td>". $vari->nota . "</td>";
+		
+?>
+@endforeach
+</tr>
+</table>
+<a href="javascript:cerrar();"><IMG SRC="/images/cerrar.png" WIDTH='50px' height='30px'></a> 
+</td> 
+</tr> 
+</table> 
+
+
+
+
+<table id="flo" style="display:none;" ALIGN="center">
+<tr>
+<td>
+<B>DEFINICIÓN</B>:
+<table WIDTH="100%">
+
+<?php
+		$vars=\sim\def::where('id_variable', '=', $variable)->take(1000)->get();
+		?>
+			@foreach ($vars as $vari)
+			<?php
+		
+		echo "<tr WIDTH='100%'><td WIDTH='50%' ALIGN='justify'>". $vari->DATO . ":</td><td WIDTH='50%' ALIGN='justify'>". $vari->DEF . "</tr></td>";			
+		
+?>
+@endforeach
+</tr>
+</table>
+<a href="javascript:cer();" ><IMG SRC="/images/cerrar.png" WIDTH='50px' height='30px'></a> 
+</td> 
+</tr> 
+</table> 
